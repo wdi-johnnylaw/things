6c6373038ff9fab4cb62d8276168130193fae347
John Lawrence <johnonrails@gmail.com>
2014-03-01 09:24:52 -0800

    Initial commit
    rails new things -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Things::Application.config.secret_key_base = 'd304811dc82afead7ee47124af7609c95d34dcd8e214e32f7ec8db2453c48334b402af61ee5a5ac255408d062b9235a934021332dcb66cea9cd7e432cc6e7256'
