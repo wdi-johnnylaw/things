86824f352a530ad8d2880bcc449b655511414106
John Lawrence <johnonrails@gmail.com>
2014-03-01 22:48:15 -0800

    Add validations for rating and comment on Opinion
    [Completes #66727664]

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -3,4 +3,11 @@ class Opinion < ActiveRecord::Base
   belongs_to :thing
 
   validates :user_id, uniqueness: { scope: [:thing_id] }
+  validates :comment, length: { minimum: 15, message: 'must be at least 15 characters' }
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    only_integer: true,
+    message: 'must be an integer between 1 and 5'
+  }
 end
--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -13,5 +13,43 @@ describe Opinion do
       before { FactoryGirl.create :opinion, user_id: opinion.user.id, thing_id: opinion.thing.id }
       it { should_not be_valid }
     end
+
+    context 'when comment is shorter than 15 characters' do
+      let(:opinion) { FactoryGirl.build :opinion, comment: '12345678901234' }
+
+      it do
+        should_not be_valid
+        expect(opinion.errors[:comment]).to include "must be at least 15 characters"
+      end
+    end
+
+    describe '.rating' do
+      subject { opinion.errors[:rating] }
+      let(:opinion) do
+        FactoryGirl.build(:opinion, rating: rating).tap do |opinion|
+          opinion.valid?
+        end
+      end
+
+      context 'when rating is not present' do
+        let(:rating) { nil }
+        it { should include "must be an integer between 1 and 5" }
+      end
+
+      context 'when rating is present but is greater than 5' do
+        let(:rating) { 6 }
+        it { should include "must be an integer between 1 and 5" }
+      end
+
+      context 'when rating is present but is less than 1' do
+        let(:rating) { 0 }
+        it { should include "must be an integer between 1 and 5" }
+      end
+
+      context 'when rating is non-integer' do
+        let(:rating) { 3.2 }
+        it { should include "must be an integer between 1 and 5" }
+      end
+    end
   end
 end
